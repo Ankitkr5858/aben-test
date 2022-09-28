1f010ccae9d5eeb6c5053825c9b354209e814794
Ankitkr5858 <ankitkr5858@gmail.com>
2022-09-28 19:48:55 +0530

    keyboard

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <keyBoard />
+</template>
+
+<script>
+import keyBoard from "./components/keyBoard.vue";
+
+export default {
+  name: "App",
+  components: {
+    keyBoard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
